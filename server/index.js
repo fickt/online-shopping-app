0baf8b0df21ee5d5d61a59f8c8803ab4450f8822
Michael Arbuzov <garrys2machinima@gmail.com>
2023-01-30 22:59:14 +0700

    routers added

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
 const start = async () => {
     try {
         console.log(process.env.DB_NAME)
-            console.log(process.env.DB_PASSWORD)
+        console.log(process.env.DB_PASSWORD)
         await sequelize.authenticate()
         await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
