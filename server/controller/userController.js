2bfd1c9c02e41bbbc66300893d72e48a4d412c67
Michael Arbuzov <garrys2machinima@gmail.com>
2023-01-30 23:23:27 +0700

    controllers added

---
+++ server/controller/userController.js
@@ -0,0 +1,13 @@
+class UserController {
+    async registration(req, res) {
+
+    }
+
+    async login(req, res) {
+        
+    }
+
+    async check(req, res) {
+    }
+}
+module.exports = new UserController()
