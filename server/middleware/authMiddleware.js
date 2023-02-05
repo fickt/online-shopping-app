70be32da7b93beb1983792839eb91e311e178ca7
Michael Arbuzov <garrys2machinima@gmail.com>
2023-02-05 21:02:28 +0700

    authMiddleWare added, userRouter method check implemented

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,18 @@
-module.exports = function(req, res, next) {
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
     if (req.method === "OPTIONS") {
         next()
     }
     try {
-
-    } catch(e) {
-        res.status(401).json({message: "Not authorized!"})
+        const token = req.headers.authorization.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({ message: "Not authorized!" })
+        }
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decodedToken
+        next()
+    } catch (e) {
+        return res.status(401).json({ message: "Not authorized!" })
     }
 }
